cb0b682e2f16e7132d7f284182bc561b0b0690e1
Wataru NAKAI <watnk0806@gmail.com>
2023-01-07 22:39:26 +0900

    Solve B03

---
+++ b03/src/main.rs
@@ -0,0 +1,21 @@
+use proconio::input;
+
+fn main() {
+    input! {
+      n: usize,
+      a: [i64; n],
+    }
+    for i in 0..n {
+        for j in 0..n {
+            for k in 0..n {
+                if i != j && j != k && k != i {
+                    if a[i] + a[j] + a[k] == 1000 {
+                        println!("Yes");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    println!("No");
+}
