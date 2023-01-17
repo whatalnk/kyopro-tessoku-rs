33aab4a7356f2a71584f3460354b9af70e115c88
Wataru NAKAI <watnk0806@gmail.com>
2023-01-17 21:26:12 +0900

    Solve B08

--- b08/src/main.rs
+++ b08/src/main.rs
@@ -9,21 +9,21 @@ fn main() {
     }
     let h = 1500;
     let w = 1500;
-    let mut grid = vec![vec![0; w + 2]; h + 2];
+    let mut grid: Vec<Vec<i64>> = vec![vec![0; w + 9]; h + 9];
     for pair in &xy {
         let (x, y) = pair;
-        grid[*y][*x] += 1;
+        grid[*x][*y] += 1;
     }
 
-    let mut grid2 = vec![vec![0; w + 2]; h + 2];
-    for i in 0..h {
-        for j in 1..=(w + 1) {
+    let mut grid2: Vec<Vec<i64>> = vec![vec![0; w + 9]; h + 9];
+    for i in 1..=h {
+        for j in 1..=w {
             grid2[i][j] = grid2[i][j - 1] + grid[i][j];
         }
     }
 
-    for j in 0..w {
-        for i in 1..=(h + 1) {
+    for j in 1..=w {
+        for i in 1..=h {
             grid2[i][j] += grid2[i - 1][j];
         }
     }
@@ -32,7 +32,7 @@ fn main() {
         let (a, b, c, d) = qq;
         println!(
             "{}",
-            grid2[*d][*c] - grid2[*d][*a - 1] - grid2[*b - 1][*c] + grid2[*b - 1][*a - 1]
+            grid2[*c][*d] - grid2[*a - 1][*d] - grid2[*c][*b - 1] + grid2[*a - 1][*b - 1]
         );
     }
 }
