9c88dd290daacb6bd86cce150dffb4e575624fb9
Wataru NAKAI <watnk0806@gmail.com>
2023-02-15 12:05:56 +0900

    Solve A22

---
+++ a22/src/main.rs
@@ -0,0 +1,18 @@
+#![allow(clippy::many_single_char_names)]
+
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [usize; n-1],
+        b: [usize; n-1],
+    }
+    let mut dp = vec![-1_000_000_000; n + 1];
+    dp[1] = 0;
+    for i in 1..n {
+        dp[a[i - 1]] = dp[a[i - 1]].max(dp[i] + 100);
+        dp[b[i - 1]] = dp[b[i - 1]].max(dp[i] + 150);
+    }
+    println!("{}", dp[n]);
+}
