75447aa3e87912c69b1d697a2ca7c1f7cb617a52
Wataru NAKAI <watnk0806@gmail.com>
2023-02-26 20:12:55 +0900

    Solve B27

---
+++ b27/src/main.rs
@@ -0,0 +1,26 @@
+#![allow(clippy::many_single_char_names)]
+
+use proconio::input;
+
+fn gcd(mut a: i64, mut b: i64) -> i64 {
+    while a >= 1 && b >= 1 {
+        if a >= b {
+            a %= b;
+        } else {
+            b %= a;
+        }
+    }
+    if a != 0 {
+        a
+    } else {
+        b
+    }
+}
+
+fn main() {
+    input! {
+        a: i64,
+        b: i64,
+    }
+    println!("{}", a * b / gcd(a, b));
+}
