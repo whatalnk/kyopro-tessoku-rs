47c31b0f08336f858a22c37447b92060b9834566
Wataru NAKAI <watnk0806@gmail.com>
2023-03-01 22:07:05 +0900

    Solve A29

---
+++ a29/src/main.rs
@@ -0,0 +1,24 @@
+#![allow(clippy::many_single_char_names)]
+
+use proconio::input;
+
+fn power(a: i64, b: i64, m: i64) -> i64 {
+    let mut p = a;
+    let mut ans = 1;
+    for i in 0..30 {
+        let d = 1 << i;
+        if (b / d) % 2 == 1 {
+            ans = (ans * p) % m;
+        }
+        p = (p * p) % m;
+    }
+    ans
+}
+
+fn main() {
+    input! {
+        a: i64,
+        b: i64,
+    }
+    println!("{}", power(a, b, 1000000007));
+}
