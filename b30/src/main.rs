b987e81fb271c33362919fb177355ce0d3e55246
Wataru NAKAI <watnk0806@gmail.com>
2023-03-04 19:24:20 +0900

    Solve B30

---
+++ b30/src/main.rs
@@ -0,0 +1,43 @@
+#![allow(clippy::many_single_char_names)]
+
+use proconio::input;
+
+fn power(a: i64, b: i64, m: i64) -> i64 {
+    let mut p = a;
+    let mut ans = 1;
+    for i in 0..30 {
+        let d = 1 << i;
+        if (b / d) % 2 == 1 {
+            ans = (ans * p) % m;
+        }
+        p = (p * p) % m;
+    }
+    ans
+}
+
+fn division(a: i64, b: i64, m: i64) -> i64 {
+    (a * power(b, m - 2, m)) % m
+}
+
+fn main() {
+    input! {
+        h: i64,
+        w: i64,
+    }
+    let m = 1_000_000_007;
+    let n = h + w - 2;
+    let r = w - 1;
+    let mut a = 1;
+    for i in 1..=n {
+        a = (a * i) % m;
+    }
+
+    let mut b = 1;
+    for i in 1..=r {
+        b = (b * i) % m;
+    }
+    for i in 1..=(n - r) {
+        b = (b * i) % m;
+    }
+    println!("{}", division(a, b, m));
+}
