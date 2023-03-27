46d00c225fa593158b44960135fd6ceb3027b6f0
Wataru NAKAI <watnk0806@gmail.com>
2023-03-27 22:59:52 +0900

    Solve B42; Editorial ver.

--- b42/src/main.rs
+++ b42/src/main.rs
@@ -8,44 +8,20 @@ fn main() {
         ab: [(i64, i64); n],
     }
     let mut answer = 0;
-    let mut asum = 0;
-    let mut bsum = 0;
-    for i in 0..n {
-        if ab[i].0 >= 0 {
-            asum += ab[i].0;
-            bsum += ab[i].1;
+    let s1 = vec![1, 1, -1, -1];
+    let s2 = vec![1, -1, -1, -1];
+    for i in 0..4 {
+        let ss1 = s1[i];
+        let ss2 = s2[i];
+        let mut asum = 0;
+        let mut bsum = 0;
+        for item in ab.iter() {
+            if ss1 * item.0 + ss2 * item.1 > 0 {
+                asum += item.0;
+                bsum += item.1;
+            }
         }
+        answer = answer.max(asum.abs() + bsum.abs());
     }
-    answer = answer.max(asum.abs() + bsum.abs());
-
-    let mut asum = 0;
-    let mut bsum = 0;
-    for i in 0..n {
-        if ab[i].1 >= 0 {
-            asum += ab[i].0;
-            bsum += ab[i].1;
-        }
-    }
-    answer = answer.max(asum.abs() + bsum.abs());
-
-    let mut asum = 0;
-    let mut bsum = 0;
-    for i in 0..n {
-        if ab[i].0 <= 0 {
-            asum += ab[i].0;
-            bsum += ab[i].1;
-        }
-    }
-    answer = answer.max(asum.abs() + bsum.abs());
-
-    let mut asum = 0;
-    let mut bsum = 0;
-    for i in 0..n {
-        if ab[i].1 <= 0 {
-            asum += ab[i].0;
-            bsum += ab[i].1;
-        }
-    }
-    answer = answer.max(asum.abs() + bsum.abs());
     println!("{}", answer);
 }
